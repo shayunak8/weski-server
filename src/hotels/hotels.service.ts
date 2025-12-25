ee94dffaf103c0f14b82f6c1c051c4dfed4827ae
shay <shayfriedman101@gmail.com>
2025-12-25 13:05:18 +0200

    feat: create hotel search service with streaming and aggregation

---
+++ src/hotels/hotels.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, Inject, Optional } from '@nestjs/common';
+import { Observable, merge, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { IHotelProvider } from './interfaces/hotel-provider.interface';
+import { HotelSearchQuery } from './interfaces/hotel-search-query.interface';
+import { Hotel } from './interfaces/hotel.interface';
+
+@Injectable()
+export class HotelsService {
+  constructor(
+    @Optional()
+    @Inject('HOTEL_PROVIDERS')
+    private readonly providers: IHotelProvider[] = [],
+  ) {}
+
+  searchHotels(query: HotelSearchQuery): Observable<Hotel> {
+    if (!this.providers || this.providers.length === 0) {
+      return of();
+    }
+
+    const groupSizes = this.generateGroupSizes(query.group_size);
+    const queries = groupSizes.map((size) => ({
+      ...query,
+      group_size: size,
+    }));
+
+    const searchObservables: Observable<Hotel>[] = [];
+
+    for (const provider of this.providers) {
+      for (const searchQuery of queries) {
+        const providerObservable = provider.searchHotels(searchQuery).pipe(
+          catchError((error) => {
+            console.error(
+              `Provider error for group_size ${searchQuery.group_size}:`,
+              error.message,
+            );
+            return of();
+          }),
+        );
+        searchObservables.push(providerObservable);
+      }
+    }
+
+    return merge(...searchObservables).pipe(map((hotel) => hotel));
+  }
+
+  private generateGroupSizes(requestedSize: number): number[] {
+    const sizes: number[] = [];
+    for (let i = requestedSize; i <= 10; i++) {
+      sizes.push(i);
+    }
+    return sizes;
+  }
+
+  getSortedHotels(hotels: Hotel[]): Hotel[] {
+    return hotels.sort((a, b) => a.price - b.price);
+  }
+}
